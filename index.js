c39221b918b171a70bfc6a49432d58aede4ed556
Andrew <andrew.thielcole@gmail.com>
2017-08-18 18:11:56 -0700

    Added url

--- index.js
+++ index.js
@@ -18,14 +18,24 @@ app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
+
 app.post('/webhook/', function (req, res) {
     let messaging_events = req.body.entry[0].messaging
     for (let i = 0; i < messaging_events.length; i++) {
 	    let event = req.body.entry[0].messaging[i]
 	    let sender = event.sender.id
-	    if (event.message && event.message.text) {
-		    let text = event.message.text
-		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+
+	    if (event.message && event.message.text && sender != myID) {
+	    	let text = event.message.text
+			request({
+			    url: 'https://ai-andrew-chatbot-flask.herokuapp.com/prediction',
+			    method: 'POST',
+			    body: {message: text.substring(0, 200)},
+			    headers: {'User-Agent': 'request'},
+				json: true 
+			}, function(error, response, body) {
+				sendTextMessage(sender, response.body)
+			})
 	    }
     }
     res.sendStatus(200)
